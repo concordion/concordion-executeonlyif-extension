6f4585e7199a7c16f924f65e2c39ea84ab88e7b7
andrew-sumner <andrew.sumner@custom.govt.nz>
2014-11-10 22:05:39 +1300

    Simplify the tests

---
+++ src/test/java/org/concordion/ext/demo/ExecuteOnlyIfEmbedded.java
@@ -0,0 +1,42 @@
+package org.concordion.ext.demo;
+
+import org.concordion.api.FailFast;
+import org.concordion.api.extension.Extensions;
+import org.concordion.ext.EmbedExtension;
+import org.concordion.ext.ExecuteOnlyIfExtension;
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+/**
+ * A fixture class for the ExecuteOnlyIf.html specification.
+ * <p>
+ * This adds the ExecuteOnlyIf Extension to Concordion to conditionally execute parts of the specification.
+ * 
+ * Also uses Embed extension
+ */
+@RunWith(ConcordionRunner.class)
+@Extensions({ EmbedExtension.class, ExecuteOnlyIfExtension.class })
+@FailFast
+public class ExecuteOnlyIfEmbedded {
+	private boolean shouldExecute = false;
+
+	public String getNotExecuteReason() {
+		String msg = "";
+
+		if(!shouldExecute) {
+			msg += "<p style=\"background-color: #FFCC99; padding: 8px; border: 1px solid #FF6600; margin: 10px 0px 10px 0px; font-weight: bold\">";
+			msg += "The following step has been skipped to prove we can.";
+			msg += "</p>";
+		}
+		
+		return msg;
+	}
+
+	public boolean shouldExecute() {
+		return shouldExecute;
+	}
+	
+	public String getCalculatorResult() {
+		return ((Integer)(6*7)).toString();
+	}
+}
